6b82f8e4d400782c78fbf8715f05a4eaf403a6a7
dfunani <dfunani@live.co.za>
2023-01-04 12:43:27 +0200

    done

--- ChelseaFC/Backend/DashboardAPI/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from DashboardAPI import views
-urlpatterns = [
-    path('clubs/<int:pk>/', views.GetClub.as_view()),
-    path('clubs/', views.GetClubList.as_view())
-]
